635cbd3077e203dca75faec1e4e324ff75f44a38
ElvisKS123 <e.shimwa2@alustudent.com>
2025-06-21 13:20:14 +0200

    MyResume

--- script.js
+++ script.js
@@ -53,5 +53,20 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = 'index.html';
         });
     }
+    // script.js
+let lastScrollY = window.scrollY;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    // Scrolling down
+    header.style.top = "-80px"; // Hide header
+  } else {
+    // Scrolling up
+    header.style.top = "0"; // Show header
+  }
+  lastScrollY = window.scrollY;
+});
+
     
 });
